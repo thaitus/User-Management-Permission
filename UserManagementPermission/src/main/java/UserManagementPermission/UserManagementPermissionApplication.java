7adcc187bea677feab091a943ab7b650e6d3eef5
thaitus <thaitu2407042@gmail.com>
2026-05-14 15:34:48 +0700

    Initial commit: Setup Spring Boot base and MySQL connection

---
+++ UserManagementPermission/src/main/java/UserManagementPermission/UserManagementPermissionApplication.java
@@ -0,0 +1,13 @@
+package UserManagementPermission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementPermissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementPermissionApplication.class, args);
+	}
+
+}
---
+++ UserManagementPermission/src/test/java/UserManagementPermission/UserManagementPermissionApplicationTests.java
@@ -0,0 +1,13 @@
+package UserManagementPermission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementPermissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
